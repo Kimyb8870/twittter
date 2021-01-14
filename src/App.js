afdd063c763c91fa5eb05db7995c0d5c3b106d4c
kimyb8870 <qhd0084@gmail.com>
2021-01-15 05:12:33 +0900

    Set: firebase setting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>Twittter</h1>
+      <h2>Work</h2>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAE85YjxzsbMQfzM_nll6gHawBC22dMowo",
+  authDomain: "twittter-e8c5e.firebaseapp.com",
+  projectId: "twittter-e8c5e",
+  storageBucket: "twittter-e8c5e.appspot.com",
+  messagingSenderId: "520525490490",
+  appId: "1:520525490490:web:6be1a2d1b8b6d5fb460803",
+  measurementId: "G-W2JJT274TL",
+};
+
+export default firebase.initializeApp(firebaseConfig);
