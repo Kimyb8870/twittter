878b8a1510f6dac46328f083d51edf6563279ab8
kimyb8870 <qhd0084@gmail.com>
2021-01-22 03:19:40 +0900

    Feature: show user's own twittts at profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,39 @@
-import React from "react";
-import { authService } from "../fbInstance";
+import React, { useState, useEffect } from "react";
+import { authService, dbService } from "../fbInstance";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const [twittts, setTwittts] = useState([]);
+
   const handleLogOut = () => {
     authService.signOut();
   };
+
+  const getMyTwittts = async () => {
+    await dbService
+      .collection("twittts")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        setTwittts(
+          snapshot.docs.map((doc) => {
+            return doc.data();
+          })
+        );
+      });
+    await console.log(twittts);
+  };
+
+  useEffect(() => {
+    getMyTwittts();
+  }, []);
 
   return (
     <div>
       <h1>Profile</h1>
       <button onClick={handleLogOut}>Log out</button>
+      {twittts.map((twittt) => (
+        <h4>{twittt.text}</h4>
+      ))}
     </div>
   );
 };
