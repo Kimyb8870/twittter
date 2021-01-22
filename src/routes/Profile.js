e870f9c523eedb72c1f929a84090ac52e498de9f
kimyb8870 <qhd0084@gmail.com>
2021-01-22 15:34:53 +0900

    Feature: update user displayname on profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -3,6 +3,7 @@ import { authService, dbService } from "../fbInstance";
 
 const Profile = ({ userObj }) => {
   const [twittts, setTwittts] = useState([]);
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
 
   const handleLogOut = () => {
     authService.signOut();
@@ -23,13 +24,37 @@ const Profile = ({ userObj }) => {
     await console.log(twittts);
   };
 
+  const handleDisplayNameChange = (e) => {
+    const { value } = e.target;
+    setNewDisplayName(value);
+  };
+
+  const handleDisplayNameSubmit = async (e) => {
+    e.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      const response = await authService.currentUser.updateProfile({
+        displayName: newDisplayName,
+      });
+      await console.log(response);
+    }
+  };
+
   useEffect(() => {
     getMyTwittts();
   }, []);
 
   return (
     <div>
-      <h1>Profile</h1>
+      <h1>Welcome {authService.currentUser.displayName}</h1>
+      <form onSubmit={handleDisplayNameSubmit}>
+        <input
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+          onChange={handleDisplayNameChange}
+        />
+        <input type="submit" value="Update profile" />
+      </form>
       <button onClick={handleLogOut}>Log out</button>
       {twittts.map((twittt) => (
         <h4>{twittt.text}</h4>
